ee3685f3f56953d7fe15f91c646d519494fea08a
rezgar muhammad <rezgar.muhammad@maibornwolff.de>
2026-02-15 15:04:10 +0100

    initialize supermarket checkout service with Angular and Spring Boot setup

---
+++ backend/src/main/java/com/haiilo/kata/checkout_service/CheckoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.haiilo.kata.checkout_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutServiceApplication.class, args);
+	}
+
+}
